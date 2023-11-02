8892890fca98e75a73b47ef831f2843689221e4f
Bhushan chougale <bhushanchougale162@gmail.com>
2023-11-02 10:11:32 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"}>
+  return (     
+    <HStack p={"4"} shadow={"base"} bgColor={"red"}>
       <Button variant={"unstyled"} color={"white"}>
         <Link to="/">Home</Link>
       </Button>
